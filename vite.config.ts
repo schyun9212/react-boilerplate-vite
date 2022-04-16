a85d5913a75acd59976d52bf1d518309d412c2ad
schyun9212 <schyun9212@gmail.com>
2022-04-17 02:37:13 +0900

    Enable emotion/css prop

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+  ],
 });
